aa6fb86db7095976342cbe9a043b9e1f4b66b7c6
Apple <apple@Apples-MacBook-Pro-98.local>
2019-07-22 12:04:22 -0700

    New Version

---
+++ Contents.swift
@@ -0,0 +1,68 @@
+import Cocoa
+
+/*var str = "Hello, playground"//
+// String- a series of characters enclosed by quotation marks
+// Integer- Whole numbers
+// Double- Ways to use decimals when coding in Swift. Use doubles for more accuracy
+// Interpolation-
+// Variable- var keyword- declare variables before use- how to hold information- substitute
+// Constant-
+// Keyword-
+// Print- sends to terminal
+var address = "13 13 Mockingbird Lane"//declare the variable address
+address = "254 Ocean Ave" //change the contents of the variable
+let name = "Karlie"
+var string = "5"
+var test = 5.5
+var new = string
+var empty:Double //data types are always written with a capital
+var printMe = "hello world"
+print (printMe)
+var program = "I'm just filling up space because I don't know how to do anything else"
+let myname = "Julia"
+var math = 78
+var time = "8:30 pm"
+time = "9.40 pm"
+print (time)
+print (myname)
+print (time)
+var hihi = "sup"
+var hh = "i'm julia"
+print (hihi + " " + hh)
+var firstname = "Julia"
+print ("Hi my name is \(firstname).")
+print (5+5)
+print ("lets do some math \(5+5).")
+print ("What is 70+9? \(70+9).")
+print ("What is 9.7+(-3)? \(9.7 + -3).")
+*/
+var temp = 87
+
+if temp > 80 {
+    print ("then wear shorts")
+}
+
+if temp < 80 {
+    print ("then wear pants")
+}
+if !(temp == 88) {
+    print ("I  correct")
+}
+else {
+}
+
+var favoriteFood = "Chipotle"
+if (favoriteFood == "Chipotle") {
+    print ("cool")
+}
+if (favoriteFood == "Starbucks") {
+    print ("awesome")
+}
+if (favoriteFood == "ramen") {
+    print ("that's nice")
+}
+if (favoriteFood == "ice cream") {
+    print ("good choice")
+} else {
+    print ("you've made a bad decision")
+}
